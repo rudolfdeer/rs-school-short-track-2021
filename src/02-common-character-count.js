d9e821b70f3a77c80a0aabcc0efc9dc2376c070d
rudolfdeer <tomalichko@gmail.com>
2021-04-06 14:35:37 +0300

    feat: implement task 2, 3

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -1,3 +1,5 @@
+// const getMatrixElementsSum = require('./01-matrix-elements-sum');
+
 /**
  * Given two strings, find the number of common characters between them.
  *
@@ -9,8 +11,17 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  let counter = 0;
+  const arr2 = s2.split('');
+
+  for (let i = 0; i < s1.length; i++) {
+    if (arr2.indexOf(s1[i]) > -1) {
+      arr2[arr2.indexOf(s1[i])] = '';
+      counter++;
+    }
+  }
+  return counter;
 }
 
 module.exports = getCommonCharacterCount;
--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,26 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const newArr = [];
+  const res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    res.push(arr[i]);
+    if (arr[i] !== -1) {
+      newArr.push(arr[i]);
+    }
+  }
+
+  newArr.sort((a, b) => b - a);
+
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] !== -1) {
+      res[i] = newArr.pop();
+    }
+  }
+
+  return res;
 }
 
 module.exports = sortByHeight;

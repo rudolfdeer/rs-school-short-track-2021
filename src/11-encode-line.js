1f58b322e5d120150b4353243bfebcdf3cafe860
rudolfdeer <tomalichko@gmail.com>
2021-04-07 13:34:25 +0300

    feat: 11-encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,35 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const counts = {};
+  const res = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (!counts[str[i]]) {
+      counts[str[i]] = 0;
+    }
+    counts[str[i]]++;
+  }
+
+  if (str[0] !== str[1]) {
+    res.push(str[0]);
+    counts[str[0]] -= 1;
+  }
+
+  Object.keys(counts).forEach((key) => {
+    if (counts[key] > 1) {
+      res.push(`${counts[key]}${key}`);
+    } else if (key !== str[0]) {
+      res.push(`${key}`);
+    }
+  });
+
+  if (str[str.length - 1] === str[0]) {
+    res.push(str[str.length - 1]);
+  }
+
+  return res.join('');
 }
 
 module.exports = encodeLine;

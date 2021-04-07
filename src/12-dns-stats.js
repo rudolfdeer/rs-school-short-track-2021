6a3eca9ed6e8be326a536b80bc344d6a9406e255
rudolfdeer <tomalichko@gmail.com>
2021-04-07 14:57:56 +0300

    feat: implement task 12-dns-stats

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,40 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const counts = {};
+
+  for (let i = 0; i < domains.length; i++) {
+    const splited = domains[i].split('.');
+    const top = `.${splited.pop()}`;
+    const second = `.${splited[splited.length - 1]}`;
+
+    let sub = '';
+
+    if (splited.length > 1) {
+      sub = `.${splited[splited.length - 2]}`;
+    }
+
+    if (!counts[top]) {
+      counts[top] = 0;
+    }
+    counts[top]++;
+
+    if (!counts[top + second]) {
+      counts[top + second] = 0;
+    }
+    counts[top + second]++;
+
+    if (!counts[top + second + sub] && sub) {
+      counts[top + second + sub] = 0;
+    }
+
+    if (sub) {
+      counts[top + second + sub]++;
+    }
+  }
+
+  return counts;
 }
 
 module.exports = getDNSStats;
